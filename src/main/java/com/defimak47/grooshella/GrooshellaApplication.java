ad65cf82605c3a66f1d467387f0cdc7c1a2afcc4
Juan y Laura <jzuriaga@imac-de-juan-zuriaga-gasco-2.local>
2016-06-26 22:17:30 +0200

    Initial spring boot starter project.

---
+++ src/main/java/com/defimak47/grooshella/GrooshellaApplication.java
@@ -0,0 +1,12 @@
+package com.defimak47.grooshella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrooshellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrooshellaApplication.class, args);
+	}
+}
